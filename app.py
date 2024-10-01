a14543c803d873bda566276172bb774cea3c1114
lucamiras <lucamiras@googlemail.com>
2024-10-01 15:06:33 +0200

    Updated tabs with more data

--- app.py
+++ app.py
@@ -11,44 +11,49 @@ da = DocumentAnalyzer("data/aresin_doc.docx", top_n=10)
     median_word_length, 
     median_sent_length, 
     median_para_length
-    ) = da.get_basic_stats()
+) = da.get_basic_stats()
 
 top_most_used, top_least_used = da.get_top_n_most_and_least_used()
 shortest_sentence, longest_sentence, shortest_word, longest_word = da.get_outliers()
+fk_grade, fk_description = da._get_flesch_kincaid_eval()
 
 headline = st.title("Prose analyzer")
 button = st.button("Analyze")
 
-metadata_tab, stats_tab, dist_tab, text_tab, quality_tab = st.tabs([
-    "Metadata",
-    "Basic Statistics",
-    "Distributions",
-    "Text Insights",
-    "Text Quality"
-])
-
-with metadata_tab:
-    
-    author = st.container()
-
-with stats_tab:
-    num_col_1, num_col_2, num_col_3, num_col_4, num_col_5 = st.columns(5)
-    num_col_6, num_col_7, num_col_8 = st.columns(3)
-    num_col_9, num_col_10, num_col_11 = st.columns(3)
-
-with dist_tab:
-    word_plot_col_1, word_plot_col_2 = st.columns(2)
-    sent_plot = st.container()
-
-with text_tab:
-    text_col = st.container()
-
-with quality_tab:
-    quality_col = st.container()
-
 if button:
+    metadata_tab, stats_tab, dist_tab, text_tab, quality_tab, content_tab = st.tabs([
+        "Metadata",
+        "Basic Statistics",
+        "Distributions",
+        "Text Insights",
+        "Text Quality",
+        "Content"
+    ])
+
+    with metadata_tab:
+        author = st.container()
+
+    with stats_tab:
+        num_col_1, num_col_2, num_col_3, num_col_4, num_col_5 = st.columns(5)
+        num_col_6, num_col_7, num_col_8 = st.columns(3)
+        num_col_9, num_col_10, num_col_11 = st.columns(3)
+
+    with dist_tab:
+        word_plot_col_1, word_plot_col_2 = st.columns(2)
+        sent_plot = st.container()
+
+    with text_tab:
+        text_col = st.container()
+
+    with quality_tab:
+        lexical_col = st.container()
+        st.divider()
+        flesh_kincaid_col = st.container()
+
+    with content_tab:
+        content_col = st.container()
+
     author.metric("Author", da.author)
-    
     num_col_1.metric("Characters", da.len_text)
     num_col_2.metric("Characters (no spaces)", da.len_text_without_spaces)
     num_col_3.metric("Words", da.num_words)
@@ -60,7 +65,7 @@ if button:
     num_col_9.metric("Median word length", median_word_length)
     num_col_10.metric("Median sentence length", median_sent_length)
     num_col_11.metric("Median paragraph length", median_para_length)
-    
+
     da.plot_word_frequency(word_plot_col_1, title="Most used words without stopwords", stopwords_removed=True, reverse=False)
     da.plot_word_frequency(word_plot_col_2, title="Least used words", stopwords_removed=True, reverse=True)
     da.plot_sentence_length(da.len_sentences, sent_plot, title="Sentence length analysis", xlabel="Sentence length distribution")
@@ -75,5 +80,12 @@ if button:
     text_col.subheader("Longest word")
     text_col.write(longest_word)
 
-    quality_col.metric("Lexical richness", da.lexical_richness)
-    quality_col.write("Lexical richness measures the number of unique words compared to the number of all words. A value closer to 1.0 indicates high lexical variety.")
+    lexical_col.metric("Lexical richness", da.lexical_richness)
+    lexical_col.write("Lexical richness measures the number of unique words compared to the number of all words. A value closer to 1.0 indicates high lexical variety.")
+    flesh_kincaid_col.metric("Flesch-Kincaid score", da.flesch_kincaid)
+    flesh_kincaid_col.markdown(f"**{fk_grade}**: {fk_description}")
+    flesh_kincaid_col.latex(r'''\text{Flesch-Kincaid Score} = 206.835 - 1.015 \left( \frac{\text{total words}}{\text{total sentences}} \right) - 84.6 \left( \frac{\text{total syllables}}{\text{total words}} \right)''')
+    
+    content_col.write(f"{len(da.chapters_dict)} chapters.")
+    for chapter, length in zip(list(da.chapters_dict.keys()), list(da.chapters_len.values())):
+        content_col.markdown(f"- {chapter} ({length} words)")
